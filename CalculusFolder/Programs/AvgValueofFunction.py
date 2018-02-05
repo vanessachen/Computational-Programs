5ee9d6388c409b7879278c0d2b28dbb543908b41
vanessachen <vanchen@nuevaschool.org>
2018-02-05 12:00:07 -0800

    fix issue with integra

---
+++ CalculusFolder/Programs/AvgValueofFunction.py
@@ -0,0 +1,41 @@
+from sympy import *
+
+x = Symbol('x')
+
+a = float(input('Input the starting value: '))
+b = float(input('Input the ending value: '))
+
+function = input("Input a function: ")
+
+integral = integrate(function)
+
+#want avg = (integral of f(x) from a to b)/b-a
+#remember : integral of f(x) from a to b = f(b) - f(a)
+f_b = integral.subs(x,b)
+f_a = integral.subs(x,a)
+
+avg_value = (f_b-f_a)/(abs(b-a))
+
+print (avg_value)
+
+
+
+
+
+
+
+
+#testing to figure out how the sympy pow object works
+#print(type(integral))
+
+#test = ((lambda x: eval(integral_str)))
+#print(test)
+
+#print(integral.subs(x,b))
+#b=x
+#print(eval(test(b)))
+#- (lambda a: eval(integral))
+
+#avg_value = dy_of_integral/(abs(b-a))
+
+#print("This is the average value: {}".format(avg_value))
--- CalculusFolder/Programs/randomTest.py
+++ CalculusFolder/Programs/randomTest.py
@@ -1,3 +1,22 @@
-import random as r
+#import random as r
 
-print(r.random())
+#print(r.random())
+
+from sympy import *
+
+x = Symbol('x')
+
+a = float(input('Input the starting value: '))
+b = int(input('Input the ending value: '))
+
+function_str = input("Input a function: ")
+
+#integral_str = integrate(function_str)
+
+function = (lambda x: eval(function_str))
+integral = integrate(function(b))
+
+print(integral)
+
+#test = ((lambda x: eval(integral_str)))
+#print(test)
